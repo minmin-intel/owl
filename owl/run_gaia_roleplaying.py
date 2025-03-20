050054b5dab8c868c053863f30ce3524063f0310
minmin-intel <minmin.hou@intel.com>
2025-03-20 21:38:23 +0000

    use together api to run gaia

--- owl/run_gaia_roleplaying.py
+++ owl/run_gaia_roleplaying.py
@@ -16,9 +16,11 @@ from camel.toolkits import (
     WebToolkit,
 )
 from camel.types import ModelPlatformType, ModelType
-from camel.configs import ChatGPTConfig
+from camel.configs import ChatGPTConfig, TogetherAIConfig
 
 from utils import GAIABenchmark
+
+import argparse
 
 
 # Configuration
@@ -28,47 +30,68 @@ test_idx = [0]
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Run GAIA benchmark')
+    parser.add_argument('--level', type=int, default=1, help='Level of the benchmark')
+    parser.add_argument('--save_result', type=bool, default=True, help='Save the result')
+    parser.add_argument("--model_provider", type=str, default="openai", help="Model provider")
+    args = parser.parse_args()
+
     """Main function to run the GAIA benchmark."""
     # Create cache directory
     cache_dir = "tmp/"
     os.makedirs(cache_dir, exist_ok=True)
 
+    if args.model_provider == "openai":
+        platform = ModelPlatformType.OPENAI
+        chat_model = ModelType.GPT_4O
+        vlm = ModelType.GPT_4O
+        model_config = ChatGPTConfig(temperature=0, top_p=1).as_dict()
+    elif args.model_provider == "together":
+        platform = ModelPlatformType.TOGETHER
+        chat_model = "meta-llama/Llama-3.3-70B-Instruct-Turbo"
+        vlm = "meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo" 
+        model_config = TogetherAIConfig(temperature=0.2).as_dict()
+    else:
+        raise ValueError(f"Model provider {args.model_provider} is not supported.")
+
+
+
     # Create models for different components
     models = {
         "user": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=chat_model,
+            model_config_dict=model_config,
         ),
         "assistant": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=chat_model,
+            model_config_dict=model_config,
         ),
         "web": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=vlm,
+            model_config_dict=model_config,
         ),
         "planning": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=chat_model,
+            model_config_dict=model_config,
         ),
         "video": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=vlm,
+            model_config_dict=model_config,
         ),
         "image": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=vlm,
+            model_config_dict=model_config,
         ),
         "search": ModelFactory.create(
-            model_platform=ModelPlatformType.OPENAI,
-            model_type=ModelType.GPT_4O,
-            model_config_dict=ChatGPTConfig(temperature=0, top_p=1).as_dict(),
+            model_platform=platform,
+            model_type=chat_model,
+            model_config_dict=model_config,
         ),
     }
     
@@ -80,8 +103,8 @@ def main():
             planning_agent_model=models["planning"],
         ).get_tools(),
         *DocumentProcessingToolkit().get_tools(),
-        *VideoAnalysisToolkit(model=models["video"]).get_tools(),  # This requires OpenAI Key
-        *AudioAnalysisToolkit().get_tools(),  # This requires OpenAI Key
+        # *VideoAnalysisToolkit(model=models["video"]).get_tools(),  # This requires OpenAI Key
+        # *AudioAnalysisToolkit().get_tools(),  # This requires OpenAI Key
         *CodeExecutionToolkit(sandbox="subprocess", verbose=True).get_tools(),
         *ImageAnalysisToolkit(model=models["image"]).get_tools(),
         *SearchToolkit(model=models["search"]).get_tools(),
